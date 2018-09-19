626d357b28c7aab64cff22cda6cf12c4b776a5cb
shioripeace <m0116225f2@edu.teu.ac.jp>
2018-09-20 08:54:58 +0900

    データベース登録のためのクラスとxml作成

---
+++ app/src/main/java/com/example/shiorinakayama/myscheduler_realm/ScheduleEditACTIVITY.kt
@@ -0,0 +1,20 @@
+package com.example.shiorinakayama.myscheduler_realm
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+import org.jetbrains.anko.startActivity
+
+class ScheduleEditACTIVITY : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_schedule_edit)
+
+
+
+
+    }
+
+
+}
